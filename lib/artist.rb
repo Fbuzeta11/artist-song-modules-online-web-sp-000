674540d212a3b8b083763feb463fc6d7c3b62420
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-29 15:09:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
